a0936be38a0486319882d0b10bbd12f5b3465066
Francislainy Campos <francislainy.campos@gmail.com>
2024-08-29 04:27:37 +0100

    Allow cross origins

--- src/main/java/com/francislainy/duchinesebe/controller/LessonController.java
+++ src/main/java/com/francislainy/duchinesebe/controller/LessonController.java
@@ -4,6 +4,7 @@ import com.francislainy.duchinesebe.service.impl.LessonServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/lessons")
 @RequiredArgsConstructor
+@CrossOrigin("*") //todo: add test https://spring.io/guides/gs/rest-service-cors - 26/08/2024
 public class LessonController {
 
     private final LessonServiceImpl lessonService;
