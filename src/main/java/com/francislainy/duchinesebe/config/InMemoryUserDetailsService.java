b498b5f5955fce39be6e64ee74868d057acc78e7
Francislainy Campos <francislainy.campos@gmail.com>
2024-09-11 04:05:08 +0100

    Add todo to explain unused class

--- src/main/java/com/francislainy/duchinesebe/config/InMemoryUserDetailsService.java
+++ src/main/java/com/francislainy/duchinesebe/config/InMemoryUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.List;
 
+//todo: unused - use it when retrieving the user from the database after it's no longer hardcoded - 2024/09/11
 @AllArgsConstructor
 public class InMemoryUserDetailsService implements UserDetailsService {
 
